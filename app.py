aeb04a3659a7e8d91e133b85a34a08e4def11604
gmautodetailing <gmautodetailing.uk@gmail.com>
2025-07-19 15:29:42 +0100

    Update app.py

--- app.py
+++ app.py
@@ -40,4 +40,6 @@ def index():
     return render_template("index.html", days=days, times=times, booked=booked_slots)
 
 if __name__ == "__main__":
-    app.run(debug=True)
+   import os
+port = int(os.environ.get("PORT", 5000))
+app.run(host="0.0.0.0", port=port)
